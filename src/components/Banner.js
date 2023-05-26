d247e4a4dc8d042e516fe2c672415d142f69f346
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-05-26 17:44:31 +0100

    installed bootstrap icons, banner

---
+++ src/components/Banner.js
@@ -0,0 +1,59 @@
+import {useState, useEffect} from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const[isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Back-end Developer", "Full Stack Developer"];
+    const[text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100); /* how fast one letter comes after the first one is typed*/
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            ticker();
+        }, delta)
+        return () => {clearInterval(ticker)}
+    }, [text])
+
+    const tick= () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length-1) : fullText.substring(0, text.length + 1);
+        setText(updatedText);
+
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if(!Deleteing && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        } else if(isDeleting && updatedText === ''{
+            setIsDeleting(false);
+        })
+    }
+
+
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-cetre">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my Portfolio</span>
+                        <h1>{`Hi I'm a webdecoded`} <span className="wrap">web developer</span></h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    )
+}
