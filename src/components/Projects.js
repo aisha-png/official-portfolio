8eae69d7baa137ced2ffee3fcc691f9f864fe388
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-05-31 14:00:48 +0100

    added linear-gradient to background

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,44 @@
-import { Container, Row } from "react-bootstrap"
-import "../styles/Projects.css"
+import { Container, Row } from "react-bootstrap";
+import "../styles/Projects.css";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+
+
 export const Projects = () => {
+
+    const projects = (
+       {
+        title: "Pokemon Project",
+        description: "Frontend Development",
+        imgUrl: projImg1,
+       }, 
+       {
+        title: "Banking API",
+        description: "Backend-project",
+        imgUrl: projImg2,
+       }, 
+       {
+        title: "Animu",
+        description: "Full Stack Project",
+        imgUrl: projImg3,
+       },
+       {
+        title: "Pokemon Project",
+        description: "Frontend Development",
+        imgUrl: projImg1,
+       }, 
+       {
+        title: "Banking API",
+        description: "Backend-project",
+        imgUrl: projImg2,
+       }, 
+       {
+        title: "Animu",
+        description: "Full Stack Project",
+        imgUrl: projImg3,
+       } 
+    )
 
     return (
         <section className="project" id="projects">
